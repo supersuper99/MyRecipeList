a0efbba212672d1821e3371517c8e92620e497de
PAPI <Eandrews100@gmail.com>
2023-01-10 12:42:49 -0800

    new login page, html home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseAuth } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          Home
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <div *ngIf="user">
+        <h1>Welcome, {{user.displayName}}!</h1>
+        <ion-button (click)="goToRecipeList()">View Recipes</ion-button>
+      </div>
+      <div *ngIf="!user">
+        <h1>Welcome to Recipe Book</h1>
+        <p>Please login to view your recipes.</p>
+        <ion-button (click)="goToLogin()">Login</ion-button>
+      </div>
+    </ion-content>
+  `
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  user: firebase.User;
+  
+  constructor(
+    private auth: FirebaseAuth,
+    private firestore: AngularFirestore,
+    private router: Router,
+  ) { }
 
-  constructor() {}
+  ngOnInit() {
+    this.auth.user.subscribe(user => {
+      this.user = user;
+    });
+  }
 
+  goToRecipeList() {
+    this.router.navigate(['/recipes']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
