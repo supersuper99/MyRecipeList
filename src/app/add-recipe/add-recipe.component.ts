fdba9d6b10f071474b0302a854474fd5b1f1580a
PAPI <Eandrews100@gmail.com>
2023-01-11 09:42:16 -0800

    ts for adding recipes public list

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-add-recipe',
-  templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.scss'],
+  templateUrl: 'add-recipe.component.html',
+  styleUrls: ['add-recipe.component.scss'],
 })
-export class AddRecipeComponent implements OnInit {
+export class AddRecipeComponent {
+  recipeName: string;
+  recipeIngredients: string;
+  recipeInstructions: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  async addRecipeToPublicList() {
+    const { recipeName, recipeIngredients, recipeInstructions } = this;
+    try {
+      const user = firebase.auth().currentUser;
+      if (!user) {
+        // Handle error for user not logged in
+        return;
+      }
 
+      const publicRecipeList = firebase.firestore().collection('publicRecipeList');
+      const recipeRef = await publicRecipeList.add({
+        name: recipeName,
+        ingredients: recipeIngredients,
+        instructions: recipeInstructions,
+        addedBy: user.uid
+      });
+      // Reset form fields
+      this.recipeName = '';
+      this.recipeIngredients = '';
+      this.recipeInstructions = '';
+    } catch (err) {
+      console.dir(err);
+      // Handle error
+    }
+  }
 }
