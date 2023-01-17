5a841a066f457cfc881bd9e366f43adb7c22ba01
PAPI <Eandrews100@gmail.com>
2023-01-16 18:26:29 -0800

    auth and firestore updating now working.

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -5,10 +5,11 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { doc, setDoc } from "firebase/firestore";
 import firebaseApp from 'src/firebase';
 
-const auth = firebase.auth();
-const db = firebase.firestore().collection('users')
+
+
 
 @Component({
   selector: 'app-signup',
@@ -16,54 +17,53 @@ const db = firebase.firestore().collection('users')
   styleUrls: ['signup.page.scss'],
 })
 export class SignupPage {
-  error: string ='';
-
+  error: string = '';
   validateForm!: FormGroup;
+  auth = firebase.auth();
+  db = firebase.firestore().collection('users')
 
 
-
-  constructor(private router: Router, private fb: FormBuilder) {}
-
-  ngOnInit(){
+  constructor(private router: Router, private fb: FormBuilder) { }
+  ngOnInit() {
     this.buildForm();
   }
 
   buildForm(): void {
     this.validateForm = this.fb.group({
-      username:[null,[Validators.maxLength(10)]],
-      email:[null,[Validators.email,Validators.required]],
-      password:[null,[Validators.required]]
+      username: [null, [Validators.maxLength(10)]],
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required]]
     });
   }
 
   signup(): void {
-    if (this.validateForm.valid){
+    if (this.validateForm.valid) {
       const email = this.validateForm.controls['email'].value;
       const password = this.validateForm.controls['password'].value;
       const username = this.validateForm.controls['username'].value;
-
-      auth.createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          const user = userCredential.user
-          userCredential.user?.updateProfile({
-            displayName: username
-          });
-          if (user) {
-          db.doc(user.uid).set({
-            username: username,
-            email: email
-          });
-        }
+      this.auth.createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        userCredential.user!.updateProfile({
+          displayName: username
+        });
+        // Add the user to the Firestore
+        this.db.doc(userCredential.user!.uid).set({
+          username: username,
+          email: email
+        });
           this.router.navigate(['/home']);
         })
-    } else{
+    } else {
       this.validateForm.markAsDirty();
     }
   }
 
-  forgotPassword(){}
 
-  
+
+
+  forgotPassword() { }
+
+
 
   // signUp(form) {
   //   const { username, email, password } = form.value;
@@ -74,13 +74,13 @@ export class SignupPage {
   //         displayName: username, photoURL:""
   //       } )
   //       .then(() => {
-     
+
   //       })
   //       .catch((error) => {
   //         // An error occurred
   //         // ...
   //       });
-       
+
   //       this.router.navigate(['/home']);
   // }})
   //     .catch((error) => {
