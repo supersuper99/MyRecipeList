ecace613b3ea6189f1c1617b5a71ae3fcfefd1ad
PAPI <Eandrews100@gmail.com>
2023-01-17 12:26:10 -0800

    recipe list test

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -5,8 +5,10 @@ import 'firebase/compat/firestore';
 import { PopoverController } from '@ionic/angular';
 import { RecipePopoverComponent } from '../recipe-popover/recipe-popover.component';
 import firebaseApp from 'src/firebase';
+import { Recipe } from '../recipe';
+import { Router } from '@angular/router';
 
-const db = firebaseApp.firestore();
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,23 +16,42 @@ const db = firebaseApp.firestore();
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  publicRecipeList: any[]= [];
-  publicRecipeListSub: any;
-  constructor(
-    public popoverController: PopoverController,
-  ) { }
+  publicRecipeList: Recipe[]= [];
+  filteredRecipes: Recipe[]= [];
+  searchTerm: string = '';
+  db = firebase.firestore();
+  auth = firebase.auth()
+
+  constructor(public popoverController: PopoverController,private router: Router) { }
+
   ngOnInit() {
-    this.publicRecipeListSub = db.collection('publicRecipeList').onSnapshot((querySnapshot) => {
+    // Get the public recipe list from Firestore
+    firebase.firestore().collection('recipes').onSnapshot((querySnapshot) => {
       this.publicRecipeList = [];
       querySnapshot.forEach((doc) => {
-        this.publicRecipeList.push({ id: doc.id, ...doc.data() });
+        this.publicRecipeList.push({
+          id: doc.id, ...doc.data(),
+          name: '',
+          ingredients: [],
+          instructions: '',
+          userId: '',
+          createdAt: new Date,
+        });
       });
+      this.filteredRecipes = this.publicRecipeList;
     });
   }
 
-  ngOnDestroy() {
-    this.publicRecipeListSub();
+  goToProfile() {
+    const user = firebase.auth().currentUser;
+    if (user) {
+      this.router.navigate(['/profile']);
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
+
+ 
 
   async showDetails(recipe: any) {
     const popover = await this.popoverController.create({
