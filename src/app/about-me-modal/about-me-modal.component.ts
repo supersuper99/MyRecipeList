640f5f6aaf14f3b88a825dcf679bbdd860bfd3c0
PAPI <Eandrews100@gmail.com>
2023-01-27 19:29:45 -0800

    fleshing out aboutme modal

--- src/app/about-me-modal/about-me-modal.component.ts
+++ src/app/about-me-modal/about-me-modal.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from 'src/services/firebase.service';
 
 @Component({
   selector: 'app-about-me-modal',
@@ -6,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me-modal.component.scss'],
 })
 export class AboutMeModalComponent implements OnInit {
+  auth = firebase.auth();
+  db = firebase.firestore();
+  validateForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.buildForm()
+  }
 
+  buildForm(): void {
+    this.validateForm = this.formBuilder.group({
+      aboutMe: ['', Validators.required],
+    });
+  }
+
+  updateProfile(){
+    const user = this.auth.currentUser;
+    const aboutMe = '';
+    
+    if (user) {
+      this.db.collection("users").doc(user.uid).update({
+        aboutme: this.user.displayName 
+      }).then(() => {
+        console.log("Document successfully updated!");
+    })
+    .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
+
+  }}
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,13 +8,15 @@ import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
 
+import { AboutMeModalComponent } from '../about-me-modal/about-me-modal.component';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,ReactiveFormsModule,FormsModule
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage,AboutMeModalComponent]
 })
 export class ProfilePageModule {}
