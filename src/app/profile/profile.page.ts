acfa19ca9bcfdd20cc20e3a31f6245693467e7f2
PAPI <Eandrews100@gmail.com>
2023-01-28 16:22:41 -0800

    displaying aboutme

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { doc } from 'firebase/firestore';
 import { FirebaseService } from 'src/services/firebase.service';
 import { AboutMeModalComponent } from '../about-me-modal/about-me-modal.component';
 
@@ -14,17 +15,36 @@ import { AboutMeModalComponent } from '../about-me-modal/about-me-modal.componen
 export class ProfilePage implements OnInit {
   auth = firebase.auth();
   db = firebase.firestore();
+  aboutMe: string ='';
 
   constructor(private firebaseService: FirebaseService, private modalCtrl: ModalController, private aboutMeModalComponent: AboutMeModalComponent) {}
 
   ngOnInit() {
-
+    this.getAboutMe()
   }
+
+  getAboutMe(){
+    const user = this.auth.currentUser
+    if(user){
+    return this.db.collection('/users').doc(user.uid).onSnapshot(doc =>  {
+      if (doc.exists) {
+          this.aboutMe = doc.data()?.['aboutMe'];
+          
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+  })
+}
+  
+  }
+
+  
   
   signout(){
   }
 
-  async aboutMe() {
+  async aboutMeModal() {
     const modal = await this.modalCtrl.create({
       component: AboutMeModalComponent,
     });
